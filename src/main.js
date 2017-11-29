1ef07a09a64f75d07ec24876763d5805cb813ba3
jingxy <953885034@qq.com>
2017-11-29 14:06:04 +0800

    富文本编辑器

--- src/main.js
+++ src/main.js
@@ -6,11 +6,18 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 import  store from "./store/index"
+import axios from 'axios';
+import './static/UE/ueditor.config'
+import './static/UE/ueditor.all.min'
+import './static/UE/lang/zh-cn/zh-cn'
+import './static/UE/ueditor.parse.min'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
+Vue.prototype.$http = axios;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
