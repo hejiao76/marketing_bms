6514a529c922e3503ff898640364c575af10ad47
jsb-cpyy <hejiao@zhidianlife.com>
2017-11-29 20:23:40 +0800

    营销管理系统---秒杀活动静态页提交

--- src/router/router_config_sedKill.js
+++ src/router/router_config_sedKill.js
@@ -5,6 +5,7 @@ const marketing_sedKill_list = r => require.ensure([], () => r(require('../page/
 const marketing_sedKill_edit = r => require.ensure([], () => r(require('../page/marketingSedKill/marketing_sedKill_edit')), 'marketing_sedKill_edit');
 const marketing_sedKill_ticket_list = r => require.ensure([], () => r(require('../page/marketingSedKill/marketing_sedKill_ticket_list.vue')), 'marketing_sedKill_ticket_list');
 const marketing_sedKill_ticket_edit = r => require.ensure([], () => r(require('../page/marketingSedKill/marketing_sedKill_ticket_edit.vue')), 'marketing_sedKill_ticket_edit');
+const marketing_sedKill_order_list = r => require.ensure([], () => r(require('../page/marketingSedKill/marketing_sedKill_order_list.vue')), 'marketing_sedKill_order_list');
 
 const routerConfig = [{
   path: '/sedkill',
@@ -12,24 +13,29 @@ const routerConfig = [{
   redirect:'/sedkill',  //根据实际业务 重定向到之路由
   children: [//二级路由  path不加 '/'  path 不加 '/'  path 不加 '/'  重要的事情说三遍  加了 '/' 代表根级
     {
-      path:'list',//公司管理员查询
+      path:'list',//秒杀活动列表
       name:"marketing_sedKill_list",
       component:marketing_sedKill_list
     },
     {
-      path:'edit/:sedKillId',//公司管理员查询
+      path:'edit/:sedKillId',//秒杀活动编辑
       name:"marketing_sedKill_edit",
       component:marketing_sedKill_edit
     },
     {
-      path:'ticket_list',//公司管理员查询
+      path:'ticket_list',//秒杀券列表
       name:"marketing_sedKill_ticket_list",
       component:marketing_sedKill_ticket_list
     },
     {
-      path:'ticket_edit/:ticketId',//公司管理员查询
+      path:'ticket_edit/:ticketId',//秒杀券编辑
       name:"marketing_sedKill_ticket_edit",
       component:marketing_sedKill_ticket_edit
+    },
+    {
+      path:'order_list/',//秒杀订单列表
+      name:"marketing_sedKill_order_list",
+      component:marketing_sedKill_order_list
     },
   ]
 }];
