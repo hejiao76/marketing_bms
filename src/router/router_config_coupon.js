7fcf9048a0a296849fe90602fe024106ac788b57
jsb-cpyy <hejiao@zhidianlife.com>
2017-12-01 11:34:51 +0800

    营销管理系统---优惠券活动静态页提交

--- src/router/router_config_coupon.js
+++ src/router/router_config_coupon.js
@@ -1,17 +1,42 @@
 
 //require.ensure()，同时将模块添加到一个分开的 chunk 当中。这个新的 chunk 会被 webpack 通过 jsonp 来按需加载。
 const marketing_coupon = r => require.ensure([], () => r(require('../page/marketingCoupon/marketing_coupon_base')), 'marketing_coupon');
+const marketing_coupon_list = r => require.ensure([], () => r(require('../page/marketingCoupon/marketing_coupon_list')), 'marketing_coupon_list');
+const marketing_coupon_edit = r => require.ensure([], () => r(require('../page/marketingCoupon/marketing_coupon_edit')), 'marketing_coupon_edit');
+const marketing_coupon_ticket_list = r => require.ensure([], () => r(require('../page/marketingCoupon/marketing_coupon_ticket_list')), 'marketing_coupon_ticket_list');
+const marketing_coupon_ticket_edit = r => require.ensure([], () => r(require('../page/marketingCoupon/marketing_coupon_ticket_edit')), 'marketing_coupon_ticket_edit');
+const marketing_coupon_record_list = r => require.ensure([], () => r(require('../page/marketingCoupon/marketing_coupon_record_list')), 'marketing_coupon_record_list');
 
 const routerConfig = [{
-  path: '/marketing_coupon',
+  path: '/coupon',
   component:marketing_coupon,
-  // redirect:'/marketing_coupon/marketing_coupon',  //根据实际业务 重定向到之路由
+   redirect:'/marketing_coupon/marketing_coupon_list',  //根据实际业务 重定向到之路由
   children: [//二级路由  path不加 '/'  path 不加 '/'  path 不加 '/'  重要的事情说三遍  加了 '/' 代表根级
     {
-      path:'marketing_coupon',//公司管理员查询
-      name:"marketing_coupon",
-      component:marketing_coupon
-    }
+      path:'list',//优惠券活动列表
+      name:"marketing_coupon_list",
+      component:marketing_coupon_list
+    },
+    {
+      path:'edit/:couponId',//优惠券活动编辑
+      name:"marketing_coupon_edit",
+      component:marketing_coupon_edit
+    },
+    {
+      path:'ticket_list',//优惠券券列表
+      name:"marketing_coupon_ticket_list",
+      component:marketing_coupon_ticket_list
+    },
+    {
+      path:'ticket_edit/:ticketId',//优惠券券编辑
+      name:"marketing_coupon_ticket_edit",
+      component:marketing_coupon_ticket_edit
+    },
+    {
+      path:'record_list/',//优惠券订单列表
+      name:"marketing_coupon_record_list",
+      component:marketing_coupon_record_list
+    },
   ]
 }];
 
